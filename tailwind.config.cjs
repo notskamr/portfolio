7151e0b1bb84eb76d1f7ed69d9546995e20f0598
Varun Sahni <66314478+notskamr@users.noreply.github.com>
2023-09-10 17:11:22 +0530

    Initial commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Montserrat", "system-ui", "sans-serif"],
+      },
+    },
+  },
+  plugins: [
+    function ({ addVariant }) {
+      addVariant("child", "& > *");
+      addVariant("child-hover", "& > *:hover");
+    },
+  ],
+};
