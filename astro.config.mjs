a4f49a399709d6ec3fe664875bae357dd84e98d9
Varun Sahni <66314478+notskamr@users.noreply.github.com>
2023-09-21 21:25:45 +0530

    robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import vercel from "@astrojs/vercel/static";
+
+import robots from "astro-robots";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), robots()],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
