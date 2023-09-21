ff7f703c0874f220c3758add9af428ffd2a4c5da
Varun Sahni <66314478+notskamr@users.noreply.github.com>
2023-09-22 00:39:02 +0530

    change now playing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 import robots from "astro-robots";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
   site: "https://vsahni.me",
   integrations: [tailwind(), robots(), sitemap()],
   adapter: vercel({
