252cdff1d7f071359b0642b424d65987125fd067
Varun Sahni <sahnivarun62@gmail.com>
2023-10-12 18:59:59 +0530

    adding marked

---
+++ src/config/Awards.ts
@@ -0,0 +1,14 @@
+interface Award {
+  icon?: string;
+  text: string;
+}
+
+export const Awards: Award[] = [
+  {
+    text: "1st Place, ShriTeq Hack-a-thon 2022",
+  },
+  {
+    text: `'The Shri Ram School, Moulsari' Innovator Award 2023 x 2`,
+  },
+  { text: "DIGEX 2023 Hi-Res (Documentary Competition) Runner's Up" },
+];
