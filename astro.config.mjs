0da3d588ae6ba713f41c40d516b3f677ca217be2
Varun Sahni <sahnivarun62@gmail.com>
2024-01-19 14:13:28 +0530

    Aria changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,21 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import robots from "astro-robots";
-
 import sitemap from "@astrojs/sitemap";
+import partytown from "@astrojs/partytown";
+
+import fontPicker from "astro-font-picker";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vsahni.me",
-  integrations: [tailwind(), robots(), sitemap()],
+  integrations: [fontPicker(), tailwind(), robots(), sitemap(), partytown()],
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
+      enabled: true
     },
     speedInsights: {
-      enabled: true,
-    },
-  }),
+      enabled: true
+    }
+  })
 });
