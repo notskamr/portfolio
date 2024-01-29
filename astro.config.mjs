cdfb017bb17a5e8f2d3347c068dd6b48c0dd99fc
Varun Sahni <sahnivarun62@gmail.com>
2024-01-29 10:06:47 +0530

    Compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,16 @@ import vercel from "@astrojs/vercel/static";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
 import partytown from "@astrojs/partytown";
-
 import fontPicker from "astro-font-picker";
+
+import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vsahni.me",
-  integrations: [fontPicker(), tailwind(), robots(), sitemap(), partytown()],
+  integrations: [fontPicker(), tailwind(), robots(), sitemap(), compress()],
   adapter: vercel({
     webAnalytics: {
-      enabled: true
-    },
-    speedInsights: {
       enabled: true
     }
   })
