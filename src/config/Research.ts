912d6413e71a8f52dbcb459e8130e77157cdd6d4
Varun Sahni <sahnivarun62@gmail.com>
2024-11-06 19:00:55 +0530

    add research

---
+++ src/config/Research.ts
@@ -0,0 +1,15 @@
+interface Research {
+    icon?: string;
+    text: string;
+    href?: string;
+    year?: string;
+    description?: string;
+}
+
+export const Research: Research[] = [
+    {
+        text: "Implementation of a Lightweight Convolutional Neural Network for the Detection of Gastrointestinal Disorders",
+        description: "Validation accuracy of 97.8% on the HyperKvasir dataset. Indexing on IEEE Xplore and Scopus is pending.",
+        year: "2024"
+    }
+]
