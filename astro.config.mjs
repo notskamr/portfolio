58867eed58fe78d6518ad3943ff12ee3a4efbcbe
Varun Sahni <sahnivarun62@gmail.com>
2024-12-06 01:03:02 +0530

    remove deprecated deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,13 @@ import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
-import fontPicker from "astro-font-picker";
 
 import compress from "astro-compress";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vsahni.me",
-  integrations: [fontPicker(), tailwind(), robots(), sitemap(), compress()],
+  integrations: [tailwind(), robots(), sitemap(), compress()],
   adapter: vercel({
     webAnalytics: {
       enabled: true,
